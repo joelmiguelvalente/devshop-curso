13722396a8b1cf24ade78b93f288270aea635b6f
Miguel92 <joelmiguelvalente@gmail.com>
2026-09-11 09:45:01 -0300

    change: Quitando clases

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -3,8 +3,8 @@ const Footer = ({ brand }) => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <footer className="py-3 bg-background-glass opacity-4">
-            <p className="footer-credits text-center py-1 text-sm text-muted border-1">
+        <footer className="py-3">
+            <p className="footer-credits text-center py-1 text-sm border-1">
                 <strong className="font-bold">{brand}</strong> &copy; {currentYear}. Todos los derechos reservados.
             </p>
         </footer>
